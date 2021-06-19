fe898f33bde66bd11dcd088682bd2afd3ae33f7c
mekeltensley <mekelintech@gmail.com>
2021-06-19 16:29:20 -0500

    Update Allowed Host

--- travel_blog/settings/production.py
+++ travel_blog/settings/production.py
@@ -4,4 +4,4 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['fierce-inlet-31772.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'fierce-inlet-31772.herokuapp.com']
