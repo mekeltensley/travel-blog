1908ab20ef4cf371b23d58c1b7bfca786dc1818f
mekeltensley <mekelintech@gmail.com>
2021-06-19 18:01:42 -0500

    Change DEBUG setting

--- travel_blog/settings.py
+++ travel_blog/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+DEBUG = False
+
 ALLOWED_HOST = ['travel-blog-us.herokuapp.com']
 
 # Application definition
