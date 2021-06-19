c05141f2cf9748b3cf71264ec48f0086711cb464
mekeltensley <mekelintech@gmail.com>
2021-06-19 16:19:41 -0500

    Reverse Database Config.

--- travel_blog/settings/production.py
+++ travel_blog/settings/production.py
@@ -1,13 +1,7 @@
 from travel_blog.settings.common import *
-import dj_database_url
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['fierce-inlet-31772.herokuapp.com']
-
-# DATABASE CONFIGURATION
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
